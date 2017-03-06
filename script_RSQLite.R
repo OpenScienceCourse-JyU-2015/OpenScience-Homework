74974b204106e6d77345f77dc1e3a6e16ac9e0c7
Gabriel <gamamo@utu.fi>
2017-03-06 17:15:25 +0200

    I created my first SQL database and used queries to split the data base in two according to Types of trees

--- script_RSQLite.R
+++ script_RSQLite.R
@@ -1,2 +1,31 @@
 # RSQLite
+dir()
+
+install.packages(c("RSQLite","DBI"))
+library(RSQLite)
+library(DBI)
+
+#creating a database
+
+mydb <- dbConnect(RSQLite::SQLite(), "")
+
+#loading the data to the database
+
+trees <- read.csv("odTrees_afterGMM.csv")
+dbWriteTable(mydb, "trees", trees)
+  dbListTables(mydb)
+
+# using queries to split the database into 2
+# the first colunm of the tree database "TYPEOFTREE" will be split in two to compose two datatables
+  
+   treesStreet <- dbGetQuery(mydb, 'SELECT * FROM trees WHERE "TYPEOFTREE" = "StreetTree"')
+   treesPark   <- dbGetQuery(mydb, 'SELECT * FROM trees WHERE "TYPEOFTREE" = "ParkTree"')
+
+#write on database
+   dbWriteTable(mydb, "treesStreet", treesStreet)
+   dbWriteTable(mydb, "treesPark", treesPark)
+   
+# disconect the database
+dbDisconnect(mydb)
+
 

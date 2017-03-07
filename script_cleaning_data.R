6d6c2553132d07ffcd4790ac18ef4e6b8848b1c4
Matthieu Bruneaux <matthieu.bruneaux@gmail.com>
2017-03-07 12:00:49 +0200

    Comments for data cleaning

--- script_cleaning_data.R
+++ script_cleaning_data.R
@@ -1,6 +1,6 @@
 
 # installing the relevant packages
-install.packages(c("tidyr","plyr"))
+#install.packages(c("tidyr","plyr"))
 library(tidyr)
 library(plyr)
 
@@ -18,45 +18,67 @@ colnames(trees)
 #replace all N/A per NA
 trees[trees=="N/A"] <- NA
 
-sort(unique(trees$SPECIES)  )
-  # replace \ for empty spaces
-  trees$SPECIES <- gsub("\"","",trees$SPECIES)
-  trees$SPECIES[grep("Abies",trees$SPECIES)][3] <- "Abies (type)"
+sort(unique(trees$SPECIES))
+# replace \ for empty spaces
+trees$SPECIES <- gsub("\"","",trees$SPECIES)
+# replace "Abies(type)" by "Abies (type)"
+## Gabriel's version:
+#trees$SPECIES[grep("Abies",trees$SPECIES)][3] <- "Abies (type)"
+## Matthieu's comment: More robust (does not rely on using the index [3])
+trees$SPECIES[grepl("Abies(type)",trees$SPECIES, fixed = T)] <- "Abies (type)"
   
 unique(trees$SPECIESTYPE)  
 
 unique(trees$AGE)   
-  # replace empty lines for NA and standardize nomenclature
-  trees$AGE[which(trees$AGE=="")] <- NA
-  trees$AGE[which(trees$AGE=="Semi-Mature")] <- "Semi Mature"
+# replace empty lines for NA and standardize nomenclature
+trees$AGE[which(trees$AGE=="")] <- NA
+trees$AGE[which(trees$AGE=="Semi-Mature")] <- "Semi Mature"
   
 unique(trees$DESCRIPTION)    
-  # standardize nomenclature
-  trees$DESCRIPTION[which(trees$DESCRIPTION=="Semi-Mature")] <- "Semi Mature"
-  trees$DESCRIPTION[which(trees$DESCRIPTION=="Juvenile/Young")] <- "Juvenile Young"
+# standardize nomenclature
+trees$DESCRIPTION[which(trees$DESCRIPTION=="Semi-Mature")] <- "Semi Mature"
+trees$DESCRIPTION[which(trees$DESCRIPTION=="Juvenile/Young")] <- "Juvenile Young"
 
 unique(trees$TREESURROUND)    
-  # replace empty lines for NA and normalize nomenclature
-  trees$TREESURROUND[which(trees$TREESURROUND=="Bramble/Briars")] <- "Bramble Briars"
+# replace empty lines for NA and normalize nomenclature
+trees$TREESURROUND[which(trees$TREESURROUND=="Bramble/Briars")] <- "Bramble Briars"
 
 unique(trees$DIAMETERinCENTIMETRES)
-  # multiply negative values per -1, since cm cannot be negative
-  trees$DIAMETERinCENTIMETRES[which(trees$DIAMETERinCENTIMETRES<0)] <- -1*trees$DIAMETERinCENTIMETRES[which(trees$DIAMETERinCENTIMETRES<0)]
+## Gabriel's version
+## multiply negative values per -1, since cm cannot be negative
+#trees$DIAMETERinCENTIMETRES[which(trees$DIAMETERinCENTIMETRES<0)] <- -1*trees$DIAMETERinCENTIMETRES[which(trees$DIAMETERinCENTIMETRES<0)]
+## Matthieu's comment:
+## The thing is that we do not really know the reason why some numbers are negative.
+## Can we really assume it is just a sign error? There are many of them.
+## The most conservative approach could be just to set those values to NA
+trees$DIAMETERinCENTIMETRES[trees$DIAMETERinCENTIMETRES<=0)] = NA
 
-unique(trees$SPREADRADIUSinMETRES)    
- # multiply negative values per -1, since negatives are not allowed
-  trees$SPREADRADIUSinMETRES[which(trees$SPREADRADIUSinMETRES<0)] <- -1*trees$SPREADRADIUSinMETRES[which(trees$SPREADRADIUSinMETRES<0)]
+unique(trees$SPREADRADIUSinMETRES)
+## Gabriel's version:
+## multiply negative values per -1, since negatives are not allowed
+#trees$SPREADRADIUSinMETRES[which(trees$SPREADRADIUSinMETRES<0)] <- -1*trees$SPREADRADIUSinMETRES[which(trees$SPREADRADIUSinMETRES<0)]
+## Matthieu's comment:
+## Same thing as for the negative diameters: let's just set those impossible values to NA
+trees$SPREADRADIUSinMETRES[trees$SPREADRADIUSinMETRES<=0] = NA
 
 unique(trees$TREETAG)
-  # replace empty lines for NA 
-  trees$TREETAG[which(trees$TREETAG=="")] <- NA
+# replace empty lines for NA 
+trees$TREETAG[which(trees$TREETAG=="")] <- NA
 
 unique(trees$TREEHEIGHTinMETRES)    
-  # multiply negative values per -1, since negatives are not allowed
-  trees$TREEHEIGHTinMETRES[which(trees$TREEHEIGHTinMETRES<0)] <- -1*trees$TREEHEIGHTinMETRES[which(trees$TREEHEIGHTinMETRES<0)]
-  # exclude bizarre value
-  trees$TREEHEIGHTinMETRES[which(trees$TREEHEIGHTinMETRES==832)] <- NA
- 
+## Gabriel's version:
+## multiply negative values per -1, since negatives are not allowed
+#trees$TREEHEIGHTinMETRES[which(trees$TREEHEIGHTinMETRES<0)] <- -1*trees$TREEHEIGHTinMETRES[which(trees$TREEHEIGHTinMETRES<0)]
+## Matthieu's comment:
+## Again, let's just set them to NA
+trees$TREEHEIGHTinMETRES[trees$TREEHEIGHTinMETRES<=0] = NA
+# exclude bizarre value
+## Gabriel's version:
+#trees$TREEHEIGHTinMETRES[which(trees$TREEHEIGHTinMETRES==832)] <- NA
+## Matthieu's comment:
+# More robust to set a threshold instead of a precise value:
+trees$TREEHEIGHTinMETRES[which(trees$TREEHEIGHTinMETRES>=200)] <- NA
+
 # end of the data cleaning  ####
 
 #the data has now been cleaned, so lets make it tidy
